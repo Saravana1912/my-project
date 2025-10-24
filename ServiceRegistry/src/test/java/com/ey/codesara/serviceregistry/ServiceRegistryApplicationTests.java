3c094c82063019d35e28d80f6b6585298c8e74a6
SM871YP <Thiyagaraja.Saravanakumar@gds.ey.com>
2025-10-24 16:49:01 +0530

    skip test classes

--- ServiceRegistry/src/test/java/com/ey/codesara/serviceregistry/ServiceRegistryApplicationTests.java
+++ ServiceRegistry/src/test/java/com/ey/codesara/serviceregistry/ServiceRegistryApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {
+@SpringBootTest(properties = {
 		"spring.cloud.config.enabled=false"
 })
 class ServiceRegistryApplicationTests {
